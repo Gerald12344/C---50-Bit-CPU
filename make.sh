4d3e913d9c920d5bd11489e27f1fc535013fd98d
Gerald12344 <harvey.randall@aol.com>
2023-02-27 17:22:50 +0000

    fixes

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ main.cpp ControlUnit.cpp SubClasses/Register.cpp SubClasses/Logger.cpp components/ALU.cpp components/GeneralPurpose.cpp components/ProgramCounter.cpp components/Out.cpp components/RAM.cpp components/GPU.cpp
+g++ main.cpp ControlUnit.cpp SubClasses/Register.cpp SubClasses/Logger.cpp components/ALU.cpp components/GeneralPurpose.cpp components/ProgramCounter.cpp components/Out.cpp components/RAM.cpp components/GPU.cpp -O3 
 ./a.out
